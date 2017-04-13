ce512ba0a1477f2411c897493ff65d9a020e3c64
maygjor <mustafa.t.elsayed@gmail.com>
2017-04-13 11:54:15 -0600

    updated

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 let http=require("http");
- function onRequest(request,response){
-  console.log(request.url);
-  response.writeHead(200,{"Content-Type" :"text/plain"});
-  response.write("connected");
+let fs=require("fs");
+
+function send404Response(response){
+  response.writeHead(404,{"Content-Type":"text/plain"});
+  response.write("404 page not found");
   response.end();
+
+}
+ function onRequest(request,response){
+  if(request.method=="GET"&&request.url=="/"){
+
+    fs.createReadStream("./index.html").pipe(response);
+  }else{
+    send404Response(response);
+  }
 
 }
 http.createServer(onRequest).listen("8080");
